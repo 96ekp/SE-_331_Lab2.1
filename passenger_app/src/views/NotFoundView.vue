9a5e43ea44fc953a12008e75bddb7c0256f590d6
Saw <ehkalanephaung96@gmail.com>
2023-07-30 01:35:30 +0700

    add default of error handling

---
+++ passenger_app/src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="not-found">
+    <h1>Page Not Found</h1>
+    <h3>The page you're looking for does not exist.</h3>
+    <router-link :to="{ name: 'home' }">Back to Home</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
