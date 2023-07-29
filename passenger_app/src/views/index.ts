729804d16ee94890fe53523d71fba642d81686fb
Saw <ehkalanephaung96@gmail.com>
2023-07-30 01:36:54 +0700

    just fix some syntal error

--- passenger_app/src/views/index.ts
+++ passenger_app/src/views/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import PassengerDetailsView from .. /views/PassengerDetailsView.vue'
+import PassengerDetailsView from ../views/PassengerDetailsView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 import NetworkErrorView from '../views/NetworkErrorView.vue'
 
