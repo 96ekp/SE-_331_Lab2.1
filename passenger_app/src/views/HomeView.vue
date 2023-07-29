6a303a4da1b5d02f578aafe1a7eaaf0f85f7117e
Saw <ehkalanephaung96@gmail.com>
2023-07-30 01:23:37 +0700

    update home view

--- passenger_app/src/views/HomeView.vue
+++ passenger_app/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Passengers</h1>
+    <ul>
+      <li v-for="passenger in passengers" :key="passenger.id">
+        <router-link :to="{ name: 'passenger-details', params: { id: passenger.id } }">
+          {{ passenger.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios'
+
+const { data: passengers } = await axios.get(
+  'https://my-json-server.typicode.com/se331-2022/passengerdb/passenger?_page=1&_limit=5'
+)
+</script>
