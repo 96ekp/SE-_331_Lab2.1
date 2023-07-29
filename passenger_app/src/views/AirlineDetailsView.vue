000eec4cffca3aeffd9e5fa77af3c9c77dc34d32
Saw <ehkalanephaung96@gmail.com>
2023-07-30 01:27:59 +0700

    created AirLineDetailView

---
+++ passenger_app/src/views/AirlineDetailsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Airline Details</h2>
+    <p>Airline Name: {{ airline.name }}</p>
+    <p>Country: {{ airline.country }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const props = defineProps(['id'])
+
+const airline = ref(null)
+
+onMounted(async () => {
+  const { data } = await axios.get(
+    `https://my-json-server.typicode.com/se331-2022/passengerdb/airline/${props.id}`
+  )
+  airline.value = data
+})
+</script>
