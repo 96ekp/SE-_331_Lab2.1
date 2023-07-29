055ad667c5a242ae66a429c3f9f951f2a6db2ddf
Saw <ehkalanephaung96@gmail.com>
2023-07-30 01:26:26 +0700

    add PassengerDetailView and import it to index.ts

--- passenger_app/src/router/index.ts
+++ passenger_app/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PassengerDetailsView from '@/views/PassengerDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,20 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/passenger/:id',
+      name: 'passenger-details',
+      component: PassengerDetailsView,
+      props: true,
+      children: [
+        {
+          path: 'airline',
+          name: 'airline-details',
+          component: AirlineDetailsView,
+          props: true
+        }
+      ]
     }
   ]
 })
---
+++ passenger_app/src/views/PassengerDetailsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Passenger Details</h1>
+    <p>Name: {{ passenger.name }}</p>
+    <p>Age: {{ passenger.age }}</p>
+    <p>Gender: {{ passenger.gender }}</p>
+
+    <router-view :passenger="passenger" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const props = defineProps(['id'])
+
+const passenger = ref(null)
+
+onMounted(async () => {
+  const { data } = await axios.get(
+    `https://my-json-server.typicode.com/se331-2022/passengerdb/passenger/${props.id}`
+  )
+  passenger.value = data
+})
+</script>
