f717ff826ddec2c91c9db8d90344c03a802f4bd7
Saw <ehkalanephaung96@gmail.com>
2023-07-30 13:34:41 +0700

    just correct the path

--- passenger_app/src/main.ts
+++ passenger_app/src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { EventItem } from '@/types'
+import { EventItem } from './types'
 
 const app = createApp(App)
 const passengers = ref<EventItem[]>([])
