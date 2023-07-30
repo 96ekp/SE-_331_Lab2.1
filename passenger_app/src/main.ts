e802c6d1ac24373326d291002be7467f9b648d58
Saw <ehkalanephaung96@gmail.com>
2023-07-30 13:30:02 +0700

    just added EventItem interface

--- passenger_app/src/main.ts
+++ passenger_app/src/main.ts
@@ -5,12 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { EventItem } from '@/types'
 
 const app = createApp(App)
-
+const passengers = ref<EventItem[]>([])
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
 const x = 'helxysx'
 console.log(x)
