90fcc41a7f7dafa1bb25e8216ea84653ccbfcf8a
Saw <ehkalanephaung96@gmail.com>
2023-07-30 12:53:43 +0700

    Requirement 5.3

--- passenger_app/src/router/index.ts
+++ passenger_app/src/router/index.ts
@@ -1,51 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PassengerDetailsView from '../views/PassengerDetailsView.vue'
 import AirlineDetailsView from '../views/AirlineDetailsView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 import NetworkErrorView from '../views/NetworkErrorView.vue'
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/passenger/:id',
+    name: 'passenger-details',
+    component: PassengerDetailsView,
+    props: true,
+    children: [
+      {
+        path: 'airline',
+        name: 'airline-details',
+        component: AirlineDetailsView,
+        props: true
+      }
+    ]
+  },
+  {
+    path: '/network-error',
+    name: 'network-error',
+    component: NetworkErrorView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: NotFoundView
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/passenger/:id',
-      name: 'passenger-details',
-      component: PassengerDetailsView,
-      props: true,
-      children: [
-        {
-          path: 'airline',
-          name: 'airline-details',
-          component: AirlineDetailsView,
-          props: true
-        }
-      ]
-    },
-    {
-      path: '/network-error',
-      name: 'network-error',
-      component: NetworkErrorView
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: 'not-found',
-      component: NotFoundView
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
